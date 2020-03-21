e2de62a5b9ac3311f3522cbb5789678f8fbe01bf
Thomas Maier <contact@thomas-maier.net>
2020-03-21 17:46:53 +0100

    WiP server component

---
+++ server/serve.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+
+import storage
+
+from bottle import route, run
+
+@route('/bundeslaender')
+def bundeslaender():
+    return storage.load_bundeslaender()
+
+@route('/rki-data-germany-sum')
+def index():
+    pass
+
+rki_data = storage.load_rki_data()
+germany_sum = {}
+for elem in rki_data:
+    datekey = elem['Meldedatum'].strftime('%Y-%m-%d')
+    if not datekey in germany_sum:
+        germany_sum[datekey] = 0
+    germany_sum[datekey] += elem['AnzahlFall']
+
+run(host='0.0.0.0', port=8080)
