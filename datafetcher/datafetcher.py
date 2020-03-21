5af8291df09c38a3c5dc564665bdcf6e9d20a086
Thomas Maier <contact@thomas-maier.net>
2020-03-21 14:21:43 +0100

    update db instead of re-creating it

--- datafetcher/datafetcher.py
+++ datafetcher/datafetcher.py
@@ -8,7 +8,7 @@ import sys
 class DataFetcher:
     def __init__(self, config):
         self.config = config
-        self.create_table_sql = """CREATE TABLE RKI_COVID19 (
+        self.CREATE_RKI_TABLE_SQL = """CREATE TABLE IF NOT EXISTS RKI_COVID19 (
             ObjectId integer PRIMARY KEY,
             Meldedatum timestamp,
             IdBundesland integer,
@@ -20,42 +20,44 @@ class DataFetcher:
             AnzahlFall integer,
             AnzahlTodesfall integer
         );"""
-        self.insert_elem_sql = """INSERT INTO RKI_COVID19 values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"""
+        self.INSERT_RKI_ELEM_SQL = """INSERT INTO RKI_COVID19 values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"""
+        self.EXISTS_RKI_ELEM_SQL = """SELECT EXISTS(SELECT 1 FROM RKI_COVID19 WHERE ObjectId=? LIMIT 1);"""
 
-    def fetch(self):
+    def fetch_rki_data(self):
         import requests
         url = self.config.get('default', 'npgeo_covid19_dataset')
         r = requests.get(url)
         if r.status_code != 200:
             print('unable to retrieve %s' % url)
             sys.exit(1)
-        self.dump(r.json())
+        self.update_rki_data(r.json())
 
-    def dump(self, json_data):
+    def db_conn(self):
         import sqlite3
-        from datetime import datetime
         db_fname = os.path.expanduser(self.config.get('default', 'coronapolation_db'))
-        try:
-            os.remove(db_fname)
-        except FileNotFoundError:
-            pass
-        conn = sqlite3.connect(db_fname)
+        return sqlite3.connect(db_fname)
+
+    def update_rki_data(self, json_data):
+        from datetime import datetime
+        conn = self.db_conn()
         cur = conn.cursor()
-        cur.execute(self.create_table_sql)
+        cur.execute(self.CREATE_RKI_TABLE_SQL)
         conn.commit()
         for elem in json_data['features']:
             data = elem['properties']
-            dt = datetime.fromisoformat(data['Meldedatum'].rstrip('Z'))
-            cur.execute(self.insert_elem_sql, (data['ObjectId'],
-                                               data['Meldedatum'],
-                                               data['IdBundesland'],
-                                               data['Bundesland'],
-                                               data['IdLandkreis'],
-                                               data['Landkreis'],
-                                               data['Altersgruppe'],
-                                               data['Geschlecht'],
-                                               data['AnzahlFall'],
-                                               data['AnzahlTodesfall']))
+            exists = cur.execute(self.EXISTS_RKI_ELEM_SQL, (data['ObjectId'],)).fetchone()[0]
+            if not exists:
+                dt = datetime.fromisoformat(data['Meldedatum'].rstrip('Z'))
+                cur.execute(self.INSERT_RKI_ELEM_SQL, (data['ObjectId'],
+                                                       data['Meldedatum'],
+                                                       data['IdBundesland'],
+                                                       data['Bundesland'],
+                                                       data['IdLandkreis'],
+                                                       data['Landkreis'],
+                                                       data['Altersgruppe'],
+                                                       data['Geschlecht'],
+                                                       data['AnzahlFall'],
+                                                       data['AnzahlTodesfall']))
         conn.commit()
         conn.close()
 
@@ -80,7 +82,7 @@ def main():
     args = get_args()
     config = get_conf(args.config)
     fetcher = DataFetcher(config)
-    fetcher.fetch()
+    fetcher.fetch_rki_data()
 
 
 if __name__ == "__main__":
