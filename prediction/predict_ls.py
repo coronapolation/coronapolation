0b2e983302d5fdafa6b7e7e2ae8da278e22ac5e6
Lucas Schweitzer <lucas.schweitzer@live.com>
2020-03-22 12:28:11 +0100

    add json to array

--- prediction/predict_ls.py
+++ prediction/predict_ls.py
@@ -1,38 +1,57 @@
 
-if False:
-    import numpy as np
-    from scipy.stats import norm
-    import matplotlib.pyplot as plt
-
-
-    # Generate some data for this demonstration.
-    data = norm.rvs(10.0, 2.5, size=500)
-
-    # Fit a normal distribution to the data:
-    mu, std = norm.fit(data)
-
-    # Plot the histogram.
-    plt.hist(data, bins=25, density=True, alpha=0.6, color='g')
-
-    # Plot the PDF.
-    xmin, xmax = plt.xlim()
-    x = np.linspace(xmin, xmax, 100)
-    p = norm.pdf(x, mu, std)
-    plt.plot(x, p, 'k', linewidth=2)
-    title = "Fit results: mu = %.2f,  std = %.2f" % (mu, std)
-    plt.title(title)
-
-    plt.show()
-
-
-
-
-
-
-
+import numpy as np
+from scipy.stats import norm
+import matplotlib.pyplot as plt
 import requests
-url = 'https://coronapolation.baremetal.rocks/infizierte/05111?since=2020-02-24'
+
+#url = 'https://coronapolation.baremetal.rocks/infizierte/05111?since=2020-02-24'
+
+
+url = 'https://coronapolation.baremetal.rocks/infizierte/05111'
 response = requests.get(url)
 resp = response.json()
 
-print('Done')
+
+rest_data = []
+
+for data in resp['days']:
+    rest_data.append(data[1])
+
+arr_data = np.asanyarray(rest_data)
+
+
+mu, std = norm.fit(arr_data)
+
+
+
+# Generate some data for this demonstration.
+data = norm.rvs(10.0, 2.5, size=500)
+
+# Fit a normal distribution to the data:
+#mu, std = norm.fit(data)
+
+# Plot the histogram.
+plt.hist(data, bins=25, density=True, alpha=0.6, color='g')
+
+# Plot the PDF.
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+p = norm.pdf(x, mu, std)
+plt.plot(x, p, 'k', linewidth=2)
+title = "Fit results: mu = %.2f,  std = %.2f" % (mu, std)
+plt.title(title)
+
+plt.show()
+
+
+
+
+
+
+
+if False:
+
+
+
+
+    print('Done')
